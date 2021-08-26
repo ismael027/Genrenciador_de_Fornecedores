8bb38701ca3d4d1173a328f19f1a0926d2e1aa2c
ismael027 <ismael.cruz@aluno.ufop.edu.br>
2021-08-26 00:48:02 -0300

    Login funcionando

--- Estoque.py
+++ Estoque.py
@@ -9,9 +9,8 @@ data = datetime.now()
 class Estoque():
     def __init__(self):
         self.Tela_Estoque = uic.loadUi("tela_estoque.ui")
-        self.Tela_CadastroEst = uic.loadUi("tela_cadastro_estoque.ui")
         self.Tela_Estoque.remover.clicked.connect(self.Excluir_Estoque)
-        self.Tela_CadastroEst.Pesquisar.clicked.connect(self.Pesquisa)
+        #self.Tela_CadastroEst.Pesquisar.clicked.connect(self.Pesquisa)
 
     def Tabela_Estoque(self):
         sql = "SELECT Q.codigo, Q.nome, Q.tipo_produto, Q.quantidade, Q.data_entrada, Q.hora FROM produto P, estoque Q where P.codigo = Q.codigo"
@@ -32,7 +31,7 @@ class Estoque():
         id_excluir = resultado[linha][0]  # Pega o codigo do produto da linha selecionada
         sql = "DELETE FROM trabalho_gestor_fornecedores.estoque WHERE id =" + str(id_excluir)
         banco.deletar(sql)
-
+    '''
     def Pesquisa(self):
         # Pega o codigo digitado e joga nos campos brancos os dados para alteração
         id_busca = self.Tela_CadastroEst.lineEdit_5.text()
@@ -44,7 +43,7 @@ class Estoque():
         self.Tela_CadastroEst.lineEdit_2.setText(str(result[0][1]))
         self.Tela_CadastroEst.lineEdit_3.setText(str(result[0][2]))
         self.Tela_CadastroEst.lineEdit_4.setText(str(result[0][3]))
-
+    '''
     def Menu_Cadastro(self):
         #Chama a tela de cadastro/alterar
         self.Tela_CadastroEst.show()
--- Main.py
+++ Main.py
@@ -1,21 +1,59 @@
 from PyQt5 import uic, QtWidgets
+from mysql.connector.utils import print_buffer
 from Produto import Produto
 from Fornecedor import Fornecedores
 from Estoque import Estoque
 from Compra import Compra
+from BD import Banco_Dados
+
+banco = Banco_Dados()
 
 app=QtWidgets.QApplication([])
 
-#telas Produto
+
+def Entrar():
+    nome = Tela_Login.lineEdit.text()
+    senha = Tela_Login.lineEdit_2.text()
+
+    if len(nome) == 0 or len(senha) == 0:
+        Tela_Login.label_4.setText("Complete todos os campos!")
+        
+    else:
+        sql = """SELECT senha FROM trabalho_gestor_fornecedores.login WHERE usuario ='"""+ nome +"""'"""
+        dados = banco.pesquisa(sql)
+        resultado = dados[0][0]
+
+        if resultado == senha:
+            Tela_Login.label_4.setText("Sucesso!")
+            Tela_Login.close()
+            Tela_Principal.show()
+ 
+        else:
+            Tela_Login.label_4.setText("Nome ou senha incorretos!")
+    '''
+def Cadastro():
+    nome = Tela_Cadastro.lineEdit.text()
+    senha = Tela_Cadastro.lineEdit_2.text()
+    confirm_senha = Tela_Cadastro.lineEdit_3.text()'''
+#Tela Login
+Tela_Login = uic.loadUi("tela_login.ui")
+Tela_Cadastro = uic.loadUi("tela_cadastro.ui")
+Tela_Login.Entrar.clicked.connect(Entrar)
+#Tela_Login.add.clicked.connect(Cadastro)
+#Tela_Cadastro.Cadastro.clicked.connect()
+Tela_Login.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+Tela_Login.show()
+
+
+#Telas Principal
 Tela_Principal = uic.loadUi("tela_principal.ui")
 
-Tela_Principal.show()
 prod = Produto()
 forn = Fornecedores()
 estoque = Estoque()
 compra = Compra()
 
-#Botões Produto
+#Botões Principal
 Tela_Principal.Botao_Produto.clicked.connect(prod.Menu_Produto)
 Tela_Principal.Botao_Fornecedor.clicked.connect(forn.Menu_Produto)
 Tela_Principal.Botao_Estoque.clicked.connect(estoque.Menu_Estoque)
