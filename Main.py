841a2e758b869e8201ec4b1795eef23b54735378
ismael027 <ismael.cruz@aluno.ufop.edu.br>
2021-08-26 01:10:15 -0300

    Cadastro criado

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 from PyQt5 import uic, QtWidgets
-from mysql.connector.utils import print_buffer
 from Produto import Produto
 from Fornecedor import Fornecedores
 from Estoque import Estoque
@@ -7,6 +6,7 @@ from Compra import Compra
 from BD import Banco_Dados
 
 banco = Banco_Dados()
+
 
 app=QtWidgets.QApplication([])
 
@@ -30,32 +30,49 @@ def Entrar():
  
         else:
             Tela_Login.label_4.setText("Nome ou senha incorretos!")
-    '''
+    
 def Cadastro():
     nome = Tela_Cadastro.lineEdit.text()
     senha = Tela_Cadastro.lineEdit_2.text()
-    confirm_senha = Tela_Cadastro.lineEdit_3.text()'''
+    confirm_senha = Tela_Cadastro.lineEdit_3.text()
+
+    if len(nome) == 0 or len(senha) == 0 or len(confirm_senha) ==0:
+        Tela_Cadastro.label_5.setText("Complete todos os campos!")
+    elif nome == senha:
+        Tela_Cadastro.label_5.setText("Insira nome e senha diferentes!")
+    elif senha != confirm_senha:
+        Tela_Cadastro.label_5.setText("Senhas diferentes!")
+    else:
+        sql = "INSERT INTO trabalho_gestor_fornecedores.login (usuario, senha) VALUES (%s, %s);"
+        values = (nome, senha)
+        banco.inserir(sql,values)
+        Tela_Cadastro.label_5.setText("Cadastro realizado com sucesso!")
+
+def Tela():
+    Tela_Cadastro.show()
+
 #Tela Login
 Tela_Login = uic.loadUi("tela_login.ui")
 Tela_Cadastro = uic.loadUi("tela_cadastro.ui")
 Tela_Login.Entrar.clicked.connect(Entrar)
-#Tela_Login.add.clicked.connect(Cadastro)
-#Tela_Cadastro.Cadastro.clicked.connect()
+Tela_Login.add.clicked.connect(Tela)
 Tela_Login.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+Tela_Cadastro.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+Tela_Cadastro.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
 Tela_Login.show()
 
-
-#Telas Principal
-Tela_Principal = uic.loadUi("tela_principal.ui")
-
+#Tela Cadastro
+Tela_Cadastro.Cadastro.clicked.connect(Cadastro)
 prod = Produto()
 forn = Fornecedores()
 estoque = Estoque()
 compra = Compra()
 
-#Botões Principal
+#Telas Principal
+Tela_Principal = uic.loadUi("tela_principal.ui")
 Tela_Principal.Botao_Produto.clicked.connect(prod.Menu_Produto)
 Tela_Principal.Botao_Fornecedor.clicked.connect(forn.Menu_Produto)
 Tela_Principal.Botao_Estoque.clicked.connect(estoque.Menu_Estoque)
 Tela_Principal.Botao_Compra.clicked.connect(compra.Menu_Compra)
+
 app.exec()
